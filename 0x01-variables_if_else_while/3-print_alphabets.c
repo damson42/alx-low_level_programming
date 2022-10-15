07fb135f71a88c65e250d921c5e9d5a01840c480
Nnadi Pedro <pedrodon247@gmail.com>
2022-10-15 00:43:44 -0700

    Betty style met

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,12 +1,12 @@
-#include <stdio.h>
-
+#include<stdio.h>
 /**
  * main - Prints the alphabet in lowercase, and then in uppercase.
  * Return: Always 0.
  */
 int main(void)
 {
-	charletter;
+	char letter;
+
 	for (letter = 'a'; letter <= 'z'; letter++)
 		putchar(letter);
 
