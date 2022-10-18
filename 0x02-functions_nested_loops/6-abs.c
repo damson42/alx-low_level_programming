45e23d6f65a2548ef841156448d3a0753d5357ae
Nnadi Pedro <pedrodon247@gmail.com>
2022-10-18 13:32:57 -0700

    Loop up

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -6,13 +6,14 @@
  */
 
 int _abs(int a)
-{	
+{
 	int k = a;
 	int absvalue;
-		if (k < 0)	
-		{		
-			k = k * (-1);	
-		}	
-			absvalue = k;	
-		return (absvalue);
+
+	if (k < 0)
+	{
+		k = k * (-1);
+	}
+		absvalue = k;
+	return (absvalue);
 }
