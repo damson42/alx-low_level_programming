ba5be5c7aa30adb6988bea3a8ee05ddee0840f86
Nnadi Pedro <pedrodon247@gmail.com>
2022-10-24 14:45:44 -0700

    Betty style met

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,33 +1,45 @@
 #include "main.h"
+
 /**
- * _atoi - changes a string to an int
- * @s: the string to be changed
- * Return: the converted int
+ * _atoi - converts a string to an integer.
+ * @s: input string.
+ * Return: integer.
 */
 int _atoi(char *s)
 {
-		int sign;
+	int i, d, n, len, f, digit;
 
-		unsigned int num;
+	i = 0;
+	d = 0;
+	n = 0;
+	len = 0;
+	f = 0;
+	digit = 0;
 
-		char *temp;
+	while (s[len] != '\0')
+		len++;
 
-		temp = s;
-		num = 0;
-		sign = 1;
-		while (*temp != '\0' && (*temp < '0' || *temp > '9'))
+	while (i < len && f == 0)
+	{
+		if (s[i] == '-')
+			++d;
+
+		if (s[i] >= '0' && s[i] <= '9')
 		{
-		if (*temp == '-')
-		{
-		sign *= -1;
+			digit = s[i] - '0';
+			if (d % 2)
+				digit = -digit;
+			n = n * 10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			f = 0;
 		}
-		temp++;
-		}
-		if (*temp != '\0')
-		{
-		do {
-		num = num * 10 + (*temp - '0');
-		temp++;
-		} while (*temp >= '0' && *temp <= '9');
-		return (num * sign);
+		i++;
+	}
+
+	if (f == 0)
+		return (0);
+
+	return (n);
 }
